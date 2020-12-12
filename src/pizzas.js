c5d11d00d29b3498c47d90542f6140536014abd2
Manuel Rodriguez <Manuel.Rodriguez@payever.org>
2020-12-12 05:35:23 +0300

    Added Menu data and images

---
+++ src/pizzas.js
@@ -0,0 +1,156 @@
+const pizzas = [
+  {
+    id: 1,
+    category: 'pizza',
+    image: '/images/pepperoni.jpg',
+    name: 'Pepperoni',
+    ingredients: 'Pepperoni, tomato sauce, mozzarella, tomatoes',
+    price: {
+      small: 8,
+      medium: 11,
+      big: 15,
+    },
+  },
+  {
+    id: 2,
+    category: 'pizza',
+    image: '/images/margherita.jpg',
+    name: 'Margherita',
+    ingredients: 'Italian herbs, tomato sauce, mozzarella, tomatoes',
+    price: {
+      small: 9,
+      medium: 11.5,
+      big: 16,
+    },
+  },
+  {
+    id: 3,
+    category: 'pizza',
+    image: '/images/carbonara.jpg',
+    name: 'Carbonara',
+    ingredients: 'Bacon, carbonara sauce, mozzarella, parmesan, tomatoes',
+    price: {
+      small: 7,
+      medium: 10,
+      big: 14,
+    },
+  },
+  {
+    id: 4,
+    category: 'pizza',
+    image: '/images/hawaiian.jpg',
+    name: 'Hawaiian',
+    ingredients: 'Chicken, tomato sauce, mozzarella, pineapple',
+    price: {
+      small: 7.5,
+      medium: 10,
+      big: 14.5,
+    },
+  },
+  {
+    id: 5,
+    category: 'pizza',
+    image: '/images/meat-lovers.jpg',
+    name: 'Meat lovers',
+    ingredients:
+      'Chicken, bacon, ham, spicy pepperoni, tomato sauce, spicy chorizo, mozzarella',
+    price: {
+      small: 10,
+      medium: 12,
+      big: 16,
+    },
+  },
+  {
+    id: 6,
+    category: 'pizza',
+    image: '/images/italian.jpg',
+    name: 'Italian',
+    ingredients:
+      'Italian herbs, spicy pepperoni, tomato sauce, mushrooms, mozzarella, black olives',
+    price: {
+      small: 8,
+      medium: 11,
+      big: 15,
+    },
+  },
+  {
+    id: 7,
+    category: 'pizza',
+    image: '/images/ham-and-mushrooms.jpg',
+    name: 'Ham and mushrooms',
+    ingredients: 'Ham, tomato sauce, champignons, mozzarella',
+    price: {
+      small: 8,
+      medium: 11,
+      big: 15,
+    },
+  },
+  {
+    id: 8,
+    category: 'pizza',
+    image: '/images/cheese-pizza.jpg',
+    name: 'Cheese pizza',
+    ingredients: 'Tomato sauce, mozzarella',
+    price: {
+      small: 8,
+      medium: 11,
+      big: 15,
+    },
+  },
+  {
+    id: 9,
+    category: 'pizza',
+    image: '/images/mexican.jpg',
+    name: 'Mexican',
+    ingredients:
+      'Chicken, tomato sauce, bell pepper, red onion, mozzarella, spicy jalapenos, tomatoes, salsa sauce',
+    price: {
+      small: 10,
+      medium: 13,
+      big: 17,
+    },
+  },
+  {
+    id: 10,
+    category: 'pizza',
+    image: '/images/chicken-pizza.jpg',
+    name: 'Chicken-pizza',
+    ingredients: 'Chicken, ham, ranch sauce, mozzarella, tomatoes, garlic',
+    price: {
+      small: 8,
+      medium: 11,
+      big: 15,
+    },
+  },
+  {
+    id: 11,
+    category: 'drinks',
+    image: '/images/coca-cola.jpg',
+    name: 'Coca-cola',
+    volume: 0.5,
+    price: {
+      small: 3,
+    },
+  },
+  {
+    id: 12,
+    category: 'drinks',
+    image: '/images/sprite.jpg',
+    name: 'Sprite',
+    volume: 0.5,
+    price: {
+      small: 3,
+    },
+  },
+  {
+    id: 13,
+    category: 'drinks',
+    image: '/images/fanta.jpg',
+    name: 'Fanta',
+    volume: 0.5,
+    price: {
+      small: 3,
+    },
+  },
+];
+export default pizzas;
