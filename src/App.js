0dffe561d6f79195b09553a17de1f14720f69bf0
Manuel Rodriguez <Manuel.Rodriguez@payever.org>
2020-12-15 03:58:39 +0300

    Added Store and Router

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Footer from './components/Footer';
 import { Container } from 'react-bootstrap';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
+import CartScreen from './screens/CartScreen';
+import ProductScreen from './screens/ProductScreen';
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
       <main className='py-3'>
         <Container>
           <Route path='/' component={HomeScreen} exact />
+          <Route path='/product/:id' component={ProductScreen} />
+          <Route path='/cart/:id?' component={CartScreen} />
         </Container>
       </main>
       <Footer />
