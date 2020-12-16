679da92739549375d962084fbe4b837b574509d2
Manuel Rodriguez <Manuel.Rodriguez@payever.org>
2020-12-16 05:36:37 +0300

    Added firebase config file

--- src/configs/firebase.config.example.js
+++ src/configs/firebase.config.example.js
@@ -1,12 +1,29 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
+import 'firebase/firestore';
 
 const firebaseConfig = {
-  // Insert your Firebase config here
+  //   Your config goes here
 };
 
 firebase.initializeApp(firebaseConfig);
 
+export const createUserDoc = async (user, contactInfo) => {
+  const userRef = firestore.doc(`users/${user.uid}`);
+  const snapshot = await userRef.get();
+  if (!snapshot.exists) {
+    const { email } = user;
+    const createdAt = new Date();
+    try {
+      userRef.set({ email, createdAt, ...contactInfo });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  return userRef;
+};
+
 export const auth = firebase.auth();
+export const firestore = firebase.firestore();
 
 export default firebase;
